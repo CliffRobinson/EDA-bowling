c5b31360adf57669f0a9b7b532cd9708088f0f94
CliffRobinson <Clifford.F.Robinson@gmail.com>
2018-05-31 10:26:19 +1200

    added perfect frame to scores

--- scores.js
+++ scores.js
@@ -10,7 +10,14 @@ var frames2 = [
   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
 ]
 
+var frames3 = [];
+
+for (let i = 0; i<10;i++){
+  frames3.push([10,10]);
+}
+
 module.exports = {
   frames1:frames1,
   frames2:frames2,
+  frames3:frames3
 }
